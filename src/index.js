90e4ada55c9768d943d3645d68a4d3b4e112b52f
jabbcode <jabbsontouch@gmail.com>
2022-01-06 13:23:46 -0400

    Creating basic server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('Hola mundo');
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+
+app.use('/', (req, res) => {
+    res.send('Bienvenido al servidor!!!')
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server on port ${ process.env.PORT }`)
+})
